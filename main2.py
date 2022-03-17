968cf4121ee2fcbcc9441ae2910105310bfb9dd4
oguz.sen <oguz.sen@outlook.com>
2022-03-17 13:50:02 +0300

    oynadım biraz

--- main2.py
+++ main2.py
@@ -2,7 +2,6 @@ from tkinter import *
 import numpy as np
 import cv2
 from PIL import Image, ImageTk
-import Resimler
 
 
 def hueminchange(v):
@@ -29,10 +28,6 @@ def vuemaxchange(v):
     global vuemax
     vuemax = int(v)
     ResmiGoster()
-def blackedToggle():
-    global blacked
-    blacked = blacked == False
-    ResmiGoster()
 def blurchange(v):
     global blur
     blur = int(v)
@@ -41,11 +36,18 @@ def consizechange(v):
     global consize
     consize = int(v)*10
     ResmiGoster()
+def treshchange(v):
+    global tresh
+    tresh = int(v)
+    ResmiGoster()
+def Yazdir():
+    global yazdırOnay
+    yazdırOnay = True
+    ResmiGoster()
 
 def MakeScaler(inframe, minimum, maximum, commandante, value, labeltext):
     ininframe = Frame(inframe, borderwidth=3, pady=10)
-    somescaler = Scale(ininframe, from_=minimum, to=maximum, orient=HORIZONTAL, command=commandante, length=240)
-
+    somescaler = Scale(ininframe,  from_=minimum, to=maximum, orient=HORIZONTAL, command=commandante, length=240)
     somescaler.set(value)
     somescaler.pack()
     someLabel = Label(ininframe, text=labeltext)
@@ -68,61 +70,66 @@ def WindowYarat():
     MakeScaler(controlframe, 0, 255, vuemaxchange, vuemax,"valuemax")
     MakeScaler(controlframe, 1, 150, blurchange, blur, "blur")
     MakeScaler(controlframe, 1, 200, consizechange, consize, "contourSizeMin*10")
-    btn_Blacked = Button(controlframe, text="Blacked", command=blackedToggle)
-    btn_Blacked.pack()
+    MakeScaler(controlframe, 0, 150, treshchange, tresh, "tresh")
+    btnyazdir = Button(controlframe, text="yazdır", command=Yazdir)
+    btnyazdir.pack()
+
+
 
 def ResmiGoster():
-    hsv = cv2.cvtColor(orjImage, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([huemin, satmin, vuemin])
-    upper_red = np.array([huemax, satmax, vuemax])
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    masked = cv2.bitwise_and(orjImage, orjImage, mask=mask)
+
+    #gerçek programda performans için bu kısım değişecek
+    blue, green, red = cv2.split(orjImage)
+    merged = cv2.merge((red, green, blue))
+    imageClone = np.array(merged[0:resyuk,0:resgen])
+
+    hsv = cv2.cvtColor(imageClone, cv2.COLOR_BGR2HSV)
+    lower_green = np.array([huemin, satmin, vuemin])
+    upper_green = np.array([huemax, satmax, vuemax])
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    masked = cv2.bitwise_and(imageClone, imageClone, mask=mask)
     gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+    blured = cv2.blur(gray, (blur,blur))
 
-        #blue, green, red = cv2.split(sonucResim)
-        #sonucResim = cv2.merge((red, green, blue))
+    ret, treshed = cv2.threshold(blured, tresh, 255, cv2.THRESH_BINARY)
 
-
-    ret, tresh = cv2.threshold(gray, 50,255, cv2.THRESH_BINARY)
-    blured = cv2.blur(tresh, (blur,blur))
-    ret, tresh = cv2.threshold(blured, 50, 255, cv2.THRESH_BINARY)
-    imageClone = np.array(tresh)
-
-    cropped = imageClone[0:700,0:1000]
+    imageClone = cv2.bitwise_and(imageClone, imageClone, mask=treshed)
 
     #cropped1 = cropped[0:50, 0:50]
     #cropped2 = cropped[50:100,0:50]
     #vis = np.concatenate((cropped1, cropped2), axis=0)
 
-    s = 100
-    for h in range(int(700/s)):
-        for w in range(int(1000/s)):
-            c = cropped[s*h:s*(h+1), s*w:s*(w+1)]
-            #print(np.average(c))
-            if np.average(c) > 40:
-                part = orjImage[s * h:s * (h + 1), s * w:s * (w + 1)]
-                cv2.imwrite("parts/utaem04_" + str(h+w) + ".jpg", part)
 
+    global yazdırOnay
 
-    im = Image.fromarray(orjImage)
+    S = 100
+    s = 50
+    if yazdırOnay:
+        for h in range(int(resyuk/s)):
+            for w in range(int(resgen/s)):
+                c = tresh[s*h:s*(h+1), s*w:s*(w+1)]
+                if np.average(c) > 60:
+                    part = orjImage[s * h:s * (h + 1), s * w:s * (w + 1)]
+                    cv2.imwrite("parts/utaem04_" + str(h+w) + ".jpg", part)
+    yazdırOnay = False
+
+    im = Image.fromarray(imageClone)
     global editedImage
     editedImage = ImageTk.PhotoImage(image=im)
     for chld in showtimeframe.winfo_children():
         chld.destroy()
     Label(showtimeframe, image=editedImage).pack()
 
-huemin = 29
+huemin = 30
 huemax = 80
 satmin = 50
 satmax = 255
 vuemin = 0
 vuemax = 255
-blacked = True
-blur = 10
+blur = 15
 consize = 25
-editedImage = None
-targetRectSize = 500
-
+tresh = 12
+yazdırOnay = False
 
 root = Tk()
 mainframe = Frame(root, border=10)
@@ -130,6 +137,12 @@ showtimeframe = Frame(mainframe)
 WindowYarat()
 
 orjImage = cv2.imread("utaem04.jpg")
-height, width, channels = orjImage.shape
+orjheight, orjwidth, orjchannels = orjImage.shape
+resgen = 1000
+resyuk=700
+
+
 ResmiGoster()
-root.mainloop()
+
+if __name__ == '__main__':
+    root.mainloop()

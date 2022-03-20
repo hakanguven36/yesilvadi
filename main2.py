5757d3d7955b1bb89cde5b7112a5ddc3662a0316
hakanguven36 <63104827+hakanguven36@users.noreply.github.com>
2022-03-20 23:31:53 +0300

    devam ediyoruuz..

--- main2.py
+++ main2.py
@@ -4,7 +4,6 @@ import cv2
 from PIL import Image, ImageTk
 import Resimler
 
-
 def hueminchange(v):
     global huemin
     huemin = int(v)
@@ -29,28 +28,32 @@ def vuemaxchange(v):
     global vuemax
     vuemax = int(v)
     ResmiGoster()
-def blackedToggle():
-    global blacked
-    blacked = blacked == False
-    ResmiGoster()
 def blurchange(v):
     global blur
     blur = int(v)
     ResmiGoster()
-def consizechange(v):
-    global consize
-    consize = int(v)*10
+
+def swapOriginal():
+    global showingOriginal
+    showingOriginal = showingOriginal is False
     ResmiGoster()
 
-def MakeScaler(inframe, minimum, maximum, commandante, value, labeltext):
-    ininframe = Frame(inframe, borderwidth=3, pady=10)
-    somescaler = Scale(ininframe, from_=minimum, to=maximum, orient=HORIZONTAL, command=commandante, length=240)
+def ResimleriHazirla():
+    global printison
+    printison = True
+    ResmiGoster()
 
+
+
+def MakeScaler(inframe, minimum, maximum, commandante, value, labeltext):
+    ininframe = Frame(inframe, borderwidth=3)
+    somescaler = Scale(ininframe, from_=minimum, to=maximum, orient=HORIZONTAL, command=commandante, length=240)
     somescaler.set(value)
-    somescaler.pack()
+    somescaler.pack(side="left")
     someLabel = Label(ininframe, text=labeltext)
-    someLabel.pack()
-    ininframe.pack()
+    someLabel.pack(side="left")
+
+    ininframe.pack(anchor="w")
 
 
 def WindowYarat():
@@ -67,44 +70,56 @@ def WindowYarat():
     MakeScaler(controlframe, 0, 255, vueminchange, vuemin,"valuemin")
     MakeScaler(controlframe, 0, 255, vuemaxchange, vuemax,"valuemax")
     MakeScaler(controlframe, 1, 150, blurchange, blur, "blur")
-    MakeScaler(controlframe, 1, 200, consizechange, consize, "contourSizeMin*10")
-    btn_Blacked = Button(controlframe, text="Blacked", command=blackedToggle)
-    btn_Blacked.pack()
+    printBtn = Button(controlframe, text="Resimleri Hazirla", command=ResimleriHazirla)
+    printBtn.pack()
+
 
 def ResmiGoster():
-    hsv = cv2.cvtColor(orjImage, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([huemin, satmin, vuemin])
-    upper_red = np.array([huemax, satmax, vuemax])
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    masked = cv2.bitwise_and(orjImage, orjImage, mask=mask)
-    gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+    imageClone = cv2.cvtColor(orjImage, cv2.COLOR_BGR2RGB)[0:resyuk, 0:resgen]
+    hsv = cv2.cvtColor(imageClone, cv2.COLOR_RGB2HSV)
+    lower_green = np.array([huemin, satmin, vuemin])
+    upper_green = np.array([huemax, satmax, vuemax])
+    onlyGreen = cv2.inRange(hsv, lower_green, upper_green)
+    blured = cv2.blur(onlyGreen, (5,5))
+    _, tresh = cv2.threshold(blured, 20,255, cv2.THRESH_BINARY)
+    masked = cv2.bitwise_and(imageClone, imageClone, mask=tresh)
 
-        #blue, green, red = cv2.split(sonucResim)
-        #sonucResim = cv2.merge((red, green, blue))
+    print(tresh.shape)
+    step = 50
+    karo = 100
+    x1 = y1 = 0
+    x2 = y2 = karo
+    global printison
+    if printison:
+        while y2 < resyuk:
+            x1 = 0
+            x2 = karo
+            while x2 < resgen:
+                parca = tresh[y1:y2, x1:x2]
+                if np.mean(parca) > 90:
+                    filename = "parts\\resim" + str(x2) + "_" + str(y2) + ".jpg"
+                    cv2.imwrite(filename, imageClone[y1:y2, x1:x2])
+                x1 += step
+                x2 += step
+            y1 += step
+            y2 += step
+        printison = False
 
 
-    ret, tresh = cv2.threshold(gray, 50,255, cv2.THRESH_BINARY)
-    blured = cv2.blur(tresh, (blur,blur))
-    ret, tresh = cv2.threshold(blured, 50, 255, cv2.THRESH_BINARY)
-    imageClone = np.array(tresh)
-
-    cropped = imageClone[0:700,0:1000]
-
-    #cropped1 = cropped[0:50, 0:50]
-    #cropped2 = cropped[50:100,0:50]
-    #vis = np.concatenate((cropped1, cropped2), axis=0)
-
-    s = 100
-    for h in range(int(700/s)):
-        for w in range(int(1000/s)):
-            c = cropped[s*h:s*(h+1), s*w:s*(w+1)]
-            #print(np.average(c))
-            if np.average(c) > 40:
-                part = orjImage[s * h:s * (h + 1), s * w:s * (w + 1)]
-                cv2.imwrite("parts/utaem04_" + str(h+w) + ".jpg", part)
 
 
-    im = Image.fromarray(orjImage)
+
+    # s = 100
+    # for h in range(int(700/s)):
+    #     for w in range(int(1000/s)):
+    #         c = cropped[s*h:s*(h+1), s*w:s*(w+1)]
+    #         #print(np.average(c))
+    #         if np.average(c) > 40:
+    #             part = orjImage[s * h:s * (h + 1), s * w:s * (w + 1)]
+    #             cv2.imwrite("parts/utaem04_" + str(h+w) + ".jpg", part)
+
+
+    im = Image.fromarray(masked)
     global editedImage
     editedImage = ImageTk.PhotoImage(image=im)
     for chld in showtimeframe.winfo_children():
@@ -117,11 +132,11 @@ satmin = 50
 satmax = 255
 vuemin = 0
 vuemax = 255
-blacked = True
-blur = 10
+blur = 4
 consize = 25
 editedImage = None
 targetRectSize = 500
+printison = False
 
 
 root = Tk()
@@ -129,7 +144,10 @@ mainframe = Frame(root, border=10)
 showtimeframe = Frame(mainframe)
 WindowYarat()
 
-orjImage = cv2.imread("utaem04.jpg")
-height, width, channels = orjImage.shape
+img = cv2.imread("210612 2012.jpg")
+orjImage = cv2.resize(img, dsize=(1000, 700), interpolation=cv2.INTER_CUBIC)
+
+resgen = 1000
+resyuk = 700
 ResmiGoster()
 root.mainloop()
